c2339a906f186b9db33a446fac3030a2cb99be5b
s1260087 <s1260087@std3dc32>
2019-07-05 11:46:24 +0900

    Initial revision

---
+++ Dicegame.py
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<time.h>
+#include <stdlib.h>
+int main(){
+    int s,t;
+    srand(time(NULL));
+    s = rand() % 6 + 1;
+    t = rand() % 6 + 1;
+    printf("Rolling the dice...\n");
+    printf("Die 1: %d\n",s);
+    printf("Die 2: %d\n",t);
+    printf("Total value: %d\n",s+t);
+    return;
+}
