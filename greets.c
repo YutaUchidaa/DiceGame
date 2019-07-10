30ba80947fcd06a33d0365aa335d23c3334df411
YutaUchidaa <52747186+YutaUchidaa@users.noreply.github.com>
2019-07-10 22:37:30 +0900

    greeting added

--- greets.c
+++ greets.c
@@ -1,9 +1,19 @@
 #include<stdio.h>
-
+#include<time.h>
+#include <stdlib.h>
 int main(){
+    
   printf("What is your name?\n>");
   char a[10];
   scanf("%s",a);
   printf("Hello,%s !\n",a);
+    int s,t;
+    srand(time(NULL));
+    s = rand() % 6 + 1;
+    t = rand() % 6 + 1;
+    printf("Rolling the dice...\n");
+    printf("Die 1: %d\n",s);
+    printf("Die 2: %d\n",t);
+    printf("Total value: %d\n",s+t);
   return;
 }
